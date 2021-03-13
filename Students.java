6e2dda138b7c46445fbc12f274052ed63e67ea6f
DarkBlueBox <80337691+DarkBlueBox@users.noreply.github.com>
2021-03-13 11:07:35 +0600

    Update Students.java

--- Students.java
+++ Students.java
@@ -3,22 +3,28 @@ package kz.aitu.oop.project;
 import java.util.Scanner;
 
 public class Students  {
-    public static int ID= (int) Math.random() ;
-   public static  String FirstName;
+
+    public static int Student_ID ;
+   public  static String FirstName;
     public static String LastName;
-    public static String Subject;
+    public static int Subject_ID;
     public Students () {
         Scanner st = new Scanner(System.in);
-System.out.println("Enter first name : ");
+        System.out.println("Enter your ID : ");
+        Student_ID = st.nextInt();
+
+        System.out.println("Enter first name : ");
 FirstName = st.next();
         System.out.println("Enter Last name : ");
         LastName = st.next();
+        System.out.println("1 - Calculus");
+        System.out.println("2 - Java");
+        System.out.println("3 - Foreign Language");
+        System.out.println("4 - Discrete math");
+        System.out.println("5 - Social-political education");
         System.out.println("Enter Subject : ");
-        Subject = st.next();
+
+        Subject_ID = st.nextInt();
     }
-    public void print (){
-        System.out.println("First Name is "+ FirstName);
-        System.out.println("Last Name is "+ LastName);
-        System.out.println("Subject is "+ Subject);
-    }
+
 }
